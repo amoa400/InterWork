1be2a8ef1611c45de40753465ca3ca124e506071
amoa400 <amoa400@gmail.com>
2013-07-07 19:07:52 +0800

    fix

--- web/demo/Public/index.php
+++ web/demo/Public/index.php
@@ -3,20 +3,4 @@
 	define('APP_NAME', 'Interview');
 	define('APP_PATH', '../Interview/');
 	define('APP_DEBUG', TRUE);
-	require '../ThinkPHP/ThinkPHP.php';
-	
-	/*
-	$_SESSION['province_id'] = 1;
-	$_SESSION['city_id'] = 1;
-
-	if ($_SESSION['login'] != 1 && !empty($_COOKIE['email']) && !empty($_COOKIE['password'])) {
-		$user = D('User')->rByEmail($_COOKIE['email']);
-		if (encrypt($_COOKIE['password']) == $user['password']) {
-			A('User')->loginSuccess($_COOKIE['email']);
-		}
-		else {
-			cookie('email', null);
-			cookie('password', null);
-		}
-	}
-	*/
+	require '../../thinkPHP/ThinkPHP.php';
